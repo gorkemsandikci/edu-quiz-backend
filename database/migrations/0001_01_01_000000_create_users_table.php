f4289623732775a7eb6b8c64131a0dd0380491ee
Gorkem Sandikci <gorkemsandikci@gmail.com>
2025-01-04 19:24:29 +0300

    fix: user migration

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -12,31 +12,29 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('users', function (Blueprint $table) {
-            $table->id(); // Birincil anahtar
-            $table->string('name'); // Kullanıcı adı
-            $table->string('email')->unique(); // E-posta adresi (uniques)
-            $table->string('ethereum_address')->unique()->nullable(false); // Ethereum adresi
-            $table->timestamp('email_verified_at')->nullable(); // E-posta doğrulama zamanı (isteğe bağlı)
-            $table->string('password'); // Şifre
-            $table->rememberToken(); // Remember me token
-            $table->timestamps(); // Oluşturulma ve güncellenme zamanları
+            $table->id();
+            $table->string('name');
+            $table->string('email')->unique();
+            $table->string('ethereum_address')->unique()->nullable(false);
+            $table->timestamp('email_verified_at')->nullable();
+            $table->string('password');
+            $table->rememberToken();
+            $table->timestamps();
         });
 
-        // Şifre sıfırlama için tokenlar tablosu
         Schema::create('password_reset_tokens', function (Blueprint $table) {
-            $table->string('email')->primary(); // E-posta adresi
-            $table->string('token'); // Token
-            $table->timestamp('created_at')->nullable(); // Token oluşturulma zamanı
+            $table->string('email')->primary();
+            $table->string('token');
+            $table->timestamp('created_at')->nullable();
         });
 
-        // Kullanıcı oturumları tablosu
         Schema::create('sessions', function (Blueprint $table) {
-            $table->string('id')->primary(); // Oturum ID'si
-            $table->foreignId('user_id')->nullable()->index(); // Kullanıcı ilişkisi
-            $table->string('ip_address', 45)->nullable(); // IP adresi
-            $table->text('user_agent')->nullable(); // Kullanıcı ajansı
-            $table->longText('payload'); // Oturum yükü
-            $table->integer('last_activity')->index(); // Son aktivite zamanı
+            $table->string('id')->primary();
+            $table->foreignId('user_id')->nullable()->index();
+            $table->string('ip_address', 45)->nullable();
+            $table->text('user_agent')->nullable();
+            $table->longText('payload');
+            $table->integer('last_activity')->index();
         });
     }
 
